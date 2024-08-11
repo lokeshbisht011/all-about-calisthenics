cfbd42667d518b9ab3d19c86dfd7a83d601568cf
Lokesh Bisht <lokeshbisht009@gmail.com>
2024-08-11 21:40:33 +0530

    corrected ga code

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,16 +19,16 @@ export default function RootLayout({ children }) {
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
+
         <Script id="ga-script" strategy="lazyOnload">
           {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
         `}
         </Script>
+
 
         <Script
           async
