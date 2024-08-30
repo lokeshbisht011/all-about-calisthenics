8a4ef0639d9399781cde93edcbcd636fcc209f7a
Lokesh Bisht <lokeshbisht009@gmail.com>
2024-08-30 22:38:44 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,22 @@ const Navbar = () => {
     <div className="bg-white border-b text-black">
       <div className="flex items-center mx-auto max-w-4xl justify-between h-16 px-5 sm:px-8 z-50">
         <Link href="/">
-          <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">AAC</div>
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="w-10 h-10 md:w-12 md:h-12"
+          />
         </Link>
         <div className="flex items-center gap-2 sm:gap-4 md:gap-5 text-sm sm:text-base md:text-lg lg:text-xl">
-          <Link href="/" className="hidden sm:inline">Home</Link>
-          <Link href="/about" className="hidden sm:inline">About</Link>
-          <Link href="/contact" className="hidden sm:inline">Contact</Link>
+          <Link href="/" className="hidden sm:inline">
+            Home
+          </Link>
+          <Link href="/about" className="hidden sm:inline">
+            About
+          </Link>
+          <Link href="/contact" className="hidden sm:inline">
+            Contact
+          </Link>
           <Link href="/glossary">Glossary</Link>
           <Link href="/one-rep-max-calculator">1RM Calculator</Link>
         </div>
