d20b484a3619bb35696262e8ae1bf59ea5c029be
Lokesh Bisht <lokeshbisht009@gmail.com>
2024-09-17 20:09:33 +0530

    added infolinks script

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,17 +29,27 @@ export default function RootLayout({ children }) {
         `}
         </Script>
 
-
+        {/* 
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4080624113623119"
           crossorigin="anonymous"
-        ></Script>
+        ></Script> */}
       </head>
       <body className={`${inter.className}`}>
         <Navbar />
         {children}
         <Footer />
+        <Script id="infolinks-script" strategy="afterInteractive">
+          {`
+            var infolinks_pid = 3425935; 
+            var infolinks_wsid = 0;
+          `}
+        </Script>
+        <Script
+          strategy="afterInteractive"
+          src="//resources.infolinks.com/js/infolinks_main.js"
+        />
       </body>
     </html>
   );
